87a84e0c8733d351f332092ab4c03952a3fc1ab9
Jayraj Rajput <jayrajput1997@gmail.com>
2024-02-07 12:28:39 +0100

    add comments to code

--- src/MLproject/pipelines/training_pipeline.py
+++ src/MLproject/pipelines/training_pipeline.py
@@ -1,35 +1,47 @@
-from src.MLproject.logger import logging
-from src.MLproject.exception import custom_exception 
-from src.MLproject.components.data_ingestion import DataIngestion, DataingestionConfig
-from src.MLproject.components.data_transformation import DataTransformationconfig, dataTransformation
-from src.MLproject.components.model_trainer import ModelTrainerconfig, ModelTrainer
+from src.MLproject.logger import logging  # Importing logging module for logging purposes
+from src.MLproject.exception import custom_exception  # Importing custom exception module
+from src.MLproject.components.data_ingestion import DataIngestion, DataingestionConfig  # Importing data ingestion components
+from src.MLproject.components.data_transformation import DataTransformationconfig, dataTransformation  # Importing data transformation components
+from src.MLproject.components.model_trainer import ModelTrainerconfig, ModelTrainer  # Importing model trainer components
 import sys,os
 
 class TrainingPipeline:
+    """
+    Class for training pipeline, which includes data ingestion, transformation, and model training.
+    """
     def __init__(self) -> None:
-        logging.info('Training pipeline ')
+        """
+        Constructor method for TrainingPipeline class.
+        """
+        logging.info('Training pipeline initialized')  # Logging initialization of training pipeline
+        # Initializing data ingestion, transformation, and model trainer components
         self.data_ingestion = DataIngestion()
         self.data_transformation = dataTransformation()
         self.model_trainer = ModelTrainer()
     
     def initiate_training(self):
+        """
+        Method to initiate training pipeline.
+        
+        Returns:
+        float: R-squared score of the trained model.
+        """
         try:
-            logging.info('Training pipline executed')
+            logging.info('Training pipeline executed')  # Logging execution of training pipeline
+            
             # Data Ingestion
-            train_data_path, test_data_path = self.data_ingestion.initiate_data_ingestion()
+            train_data_path, test_data_path = self.data_ingestion.initiate_data_ingestion()  # Initiating data ingestion
             
             # Data Transformation
-            train_data, test_data, _ = self.data_transformation.initiate_data_transformation(train_data_path, test_data_path)
+            train_data, test_data, _ = self.data_transformation.initiate_data_transformation(train_data_path, test_data_path)  # Initiating data transformation
             
             # Model Training
-            trained_model_R2_score = self.model_trainer.initiate_model_trainer(train_data=train_data, test_data=test_data)
+            trained_model_R2_score = self.model_trainer.initiate_model_trainer(train_data=train_data, test_data=test_data)  # Initiating model training
             
             # Optionally, you can add evaluation or further steps here
             
-            return trained_model_R2_score # or any other information you want to return
+            return trained_model_R2_score  # Returning R-squared score of the trained model or any other information
             
         except Exception as e:
-            logging.info ("Custom Exception ")
-            raise custom_exception(e,sys)
-            
-       
+            logging.error("Custom Exception occurred")  # Logging custom exception
+            raise custom_exception(e, sys)  # Raising custom exception with error information
--- src/MLproject/utils.py
+++ src/MLproject/utils.py
@@ -1,72 +1,74 @@
 import  os
 import sys
 import dill
-from src.MLproject.exception import custom_exception
-from src.MLproject.logger import logging
+from src.MLproject.exception import custom_exception  # Importing custom exception module
+from src.MLproject.logger import logging  # Importing logging module
 import pandas as pd 
-from dotenv import load_dotenv
-import pymysql
+from dotenv import load_dotenv  # Importing dotenv module to load environment variables
+import pymysql  # Importing pymysql for MySQL database connection
 
-from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-from sklearn.model_selection import RandomizedSearchCV,GridSearchCV
-import pickle
-import numpy 
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error  # Importing evaluation metrics
+from sklearn.model_selection import RandomizedSearchCV,GridSearchCV  # Importing model selection utilities
+import pickle  # Importing pickle module for serialization
+import numpy  # Importing numpy
 
-load_dotenv()
+load_dotenv()  # Loading environment variables from .env file
 
-host = os.getenv('host')
-user = os.getenv('user')
-password= os.getenv('password')
-db = os.getenv('db')
+host = os.getenv('host')  # Retrieving host from environment variables
+user = os.getenv('user')  # Retrieving user from environment variables
+password= os.getenv('password')  # Retrieving password from environment variables
+db = os.getenv('db')  # Retrieving database name from environment variables
 
 def read_sql_data():
+    """
+    Function to read data from SQL database.
+    """
     try:
         mydb = pymysql.connect(host=host,
                                user=user,
                                password=password,
                                db=db)
         
+        logging.info('Connection established')  # Logging successful connection
         
-        logging.info('Connection established')
+        df = pd.read_sql_query('select * from studentsperformance ', mydb)  # Reading data from SQL database
         
-        
-        df = pd.read_sql_query('select * from studentsperformance ', mydb)
-        
-        print(df.head())
+        print(df.head())  # Displaying the first few rows of the dataframe
         return df
     except Exception as e:
-        raise custom_exception(e,sys)
+        raise custom_exception(e,sys)  # Handling exceptions with custom exception module
 
 
 def save_obj(file_path, obj):
+    """
+    Function to save object to a file using pickle serialization.
+    """
     try:
         dir_path = os.path.dirname(file_path)
         
-        os.makedirs(dir_path,exist_ok=True)
-        
+        os.makedirs(dir_path,exist_ok=True)  # Creating directory if not exists
         
         with open(file_path,'wb') as f:
-            pickle.dump(obj,f)
-            
+            pickle.dump(obj,f)  # Serializing object and saving to file
     except Exception as e :
-        raise custom_exception(e,sys)  
-            
-            
+        raise custom_exception(e,sys)  # Handling exceptions with custom exception module
+
+
 def load_obj(file_path):
+    """
+    Function to load object from file using pickle deserialization.
+    """
     try:
-        
         with open(file_path,'rb') as obj:
-            return dill.load(obj)
-               
+            return dill.load(obj)  # Deserializing object from file
     except Exception as e:
-        raise custom_exception(e,sys)
+        raise custom_exception(e,sys)  # Handling exceptions with custom exception module
             
-            
-    
-    
-    
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    """
+    Function to evaluate models using specified parameters.
+    """
     try:
         report = {}
 
@@ -74,26 +76,20 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
+            gs = GridSearchCV(model,para,cv=3)  # Performing grid search cross-validation
             gs.fit(X_train,y_train)
 
-            model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)  # Setting best parameters found during grid search
+            model.fit(X_train,y_train)  # Training model
 
-            #model.fit(X_train, y_train)  # Train model
+            y_train_pred = model.predict(X_train)  # Making predictions on training data
+            y_test_pred = model.predict(X_test)  # Making predictions on test data
 
-            y_train_pred = model.predict(X_train)
+            train_model_score = r2_score(y_train, y_train_pred)  # Computing R-squared score for training data
+            test_model_score = r2_score(y_test, y_test_pred)  # Computing R-squared score for test data
 
-            y_test_pred = model.predict(X_test)
+            report[list(models.keys())[i]] = test_model_score  # Storing test model score in report dictionary
 
-            train_model_score = r2_score(y_train, y_train_pred)
-
-            test_model_score = r2_score(y_test, y_test_pred)
-
-            report[list(models.keys())[i]] = test_model_score
-
-        return report
+        return report  # Returning evaluation report
     except Exception as e:
-        raise custom_exception(e, sys)
-    
-    
+        raise custom_exception(e, sys)  # Handling exceptions with custom exception module
